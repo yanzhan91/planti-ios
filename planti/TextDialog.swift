38858b90c164010551666a6afa9912875f53ecb7
Yan Zhan <yanzhan91@gmail.com>
2019-03-05 00:18:27 -0600

    Add menu controller

---
+++ planti/TextDialog.swift
@@ -0,0 +1,44 @@
+//
+//  TextDialog.swift
+//  planti
+//
+//  Created by Zhiyi Yang on 3/4/19.
+//  Copyright © 2019 planti. All rights reserved.
+//
+
+import UIKit
+
+class TextDialog: UIView {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var text: UITextView!
+    @IBOutlet weak var closeButton: ThemeButton!
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    private func initialize() {
+        Bundle.main.loadNibNamed("TextDialogView", owner: self, options: nil)
+        self.closeButton.activate()
+        addSubview(self.contentView)
+    }
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
